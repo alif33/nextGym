beeea8ea9f23fb73fc28897cf567cea56fa46105
alif33 <alifhasan332@gmail.com>
2022-05-10 22:41:44 +0600

    ..

--- store/cart/slice.js
+++ store/cart/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import _ from "lodash";
 
 export const cartSlice = createSlice({
     name: 'cart',
@@ -6,8 +7,14 @@ export const cartSlice = createSlice({
         cartList: [],
     },
     reducers: {
+    
         setCart: (state, action) => {
-            state.cartList.push(action.payload)
+            state.cartList.push(action.payload);
+        },
+
+        setCart: (state, action) => {
+            const { index, qty } = action.payload;
+            state.cartList[index].qty = qty;
         }
     }
 })
