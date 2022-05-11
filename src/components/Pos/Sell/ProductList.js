4d38272833d3a08b1ca6308bead79b66e4565688
Jahid Hasan <jahidhasan2856@gmail.com>
2022-05-11 09:03:37 +0600

    pos product increase and decrease with grandtotal calculate

--- src/components/Pos/Sell/ProductList.js
+++ src/components/Pos/Sell/ProductList.js
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Plus, Minus } from "react-feather";
 import { useDispatch, useSelector } from 'react-redux';
-import { increaseCart, setCart } from '../../../../store/cart/actions';
+import { increaseCart, setCart, decreaseCart } from '../../../../store/cart/actions';
 import { firstNWord } from '../../../../__lib__/helpers/Validator';
+import NumberFormat from 'react-number-format';
 
-
-const ProductList = ({ cart, i }) => {
+const ProductList = ({ cart, index }) => {
     const { name, price } = cart.data;
-
     const dispatch = useDispatch()
-    const { carts } = useSelector(state => state)
+
 
     return (
         <>
             <tr>
                 <td>{firstNWord(name, 2)}</td>
-                <td>${price}</td>
+                <td>
+                    <NumberFormat value={price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                </td>
                 <td className='d-flex align-items-center justify-content-between'>
 
                     <span
-                        onClick={() => dispatch(increaseCart(i))}
+                        onClick={() => dispatch(increaseCart(index))}
                         className='btn  bg-primary text-white p-25 rounded m-25'>
                         <Plus size={12} />
                     </span>
@@ -27,14 +28,16 @@ const ProductList = ({ cart, i }) => {
                         className='btn btn-outline-primary  p-25 rounded m-25'>
                         {cart.qty}
                     </span>
-                    <span onClick={() => setQuantity(quantity > 1 ? quantity - 1 : quantity)}
+                    <span onClick={() => cart.qty > 1 && dispatch(decreaseCart(index))}
                         className='btn bg-primary text-white p-25 rounded m-25'>
                         <Minus size={12} />
                     </span>
 
                 </td>
-                
-                <td>${cart.qty * price}</td>
+
+                <td>
+                    <NumberFormat value={cart.subTotal} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                </td>
             </tr>
         </>
     );
--- src/components/Pos/Sell/TDS.js
+++ src/components/Pos/Sell/TDS.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { User, Percent, DollarSign } from "react-feather";
+import NumberFormat from 'react-number-format';
 import NumberInput from './NumberInput';
+import _ from 'lodash'
+import { useDispatch, useSelector } from 'react-redux';
 const TDS = ({ handleForm }) => {
+    const dispatch = useDispatch()
+    const { carts } = useSelector(state => state)
+    console.log(carts.cartList)
 
+    const grandTotal = _.sumBy(carts.cartList, cart =>  { return cart.subTotal; }); 
+
+    console.log(grandTotal)
 
     return (
         <>
             <div className="">
-                <h4 className="text-center bg-primary p-50 rounded text-white">Grand Total : $00.0</h4>
+                <h4 className="text-center bg-primary p-50 rounded text-white">Grand Total : 
+                <NumberFormat value={grandTotal} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                </h4>
                 <div className="row">
                     <NumberInput icon={"%"} name="TAX" handleForm={handleForm} />
                     <NumberInput icon={"$"} name="Discount" handleForm={handleForm} />
